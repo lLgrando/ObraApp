2fe7bc104f200f7a51ce5695f4ef2ac4741af787
Jenyfer Oliveira <jenyfer_kellyane@hotmail.com>
2023-03-04 15:32:56 -0300

    Criação do dto, model e repositorry

---
+++ src/main/java/com/ObrasApp/ObraApp/model/Imovel.java
@@ -0,0 +1,21 @@
+package com.ObrasApp.ObraApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Imovel {
+    @Id
+    private String id;
+    private String nomeImovel;
+    private String cidade;
+    private Float area;
+    private Double preco;
+    private Integer dormitorio;
+
+
+}
