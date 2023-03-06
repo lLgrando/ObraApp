f0c6697f62307e94b19d44d5af863da8963f0042
Pedro <phwoglo@alu.ufc.br>
2023-03-05 23:18:15 -0300

    Atualização dos controllers

--- src/main/java/com/ObrasApp/ObraApp/controller/HomeController.java
+++ src/main/java/com/ObrasApp/ObraApp/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.ObrasApp.ObraApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ObrasApp.ObraApp.service.ImovelService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
     
+    @Autowired
+    private final ImovelService imovelService;
+    
     @RequestMapping("/")
-    public String home(){
+    public String index(Model model){
+        model.addAttribute("imoveis", imovelService.ObterTodos());
         return "index";
     }
 }
