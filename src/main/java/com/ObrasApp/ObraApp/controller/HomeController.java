94f8cffac24369f013b07636b61a4575c11683a1
Pedro <phwoglo@alu.ufc.br>
2023-03-08 15:34:08 -0300

    HomeController

--- src/main/java/com/ObrasApp/ObraApp/controller/HomeController.java
+++ src/main/java/com/ObrasApp/ObraApp/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ObrasApp.ObraApp.model.Imovel;
 import com.ObrasApp.ObraApp.service.ImovelService;
